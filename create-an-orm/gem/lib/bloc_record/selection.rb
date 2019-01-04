7a5a472361a1e017246e4f09ba58d6414aeca26d
Yevgeniy Ivanov <anigeka@gmail.com>
2019-01-04 13:46:23 -0600

    checkpoint complete

--- create-an-orm/gem/lib/bloc_record/selection.rb
+++ create-an-orm/gem/lib/bloc_record/selection.rb
@@ -1,7 +1,21 @@
 require 'sqlite3'
 
 module Selection
-	def find(id)
+	def find(*ids)
+ 
+     if ids.length == 1
+       find_one(ids.first)
+     else
+       rows = connection.execute <<-SQL
+         SELECT #{columns.join ","} FROM #{table}
+         WHERE id IN (#{ids.join(",")});
+       SQL
+ 
+       rows_to_array(rows)
+     end
+    end
+
+	def find_one(id)
 		row = connection.get_first_row <<-SQL
 			SELECT #{columns.join ","} FROM #{table}
 			WHERE id = #{id};
@@ -10,4 +24,11 @@ module Selection
 		data = Hash[columns.zip(row)]
 		new(data)
 	end
+
+
+   private
+ 
+   def rows_to_array(rows)
+     rows.map { |row| new(Hash[columns.zip(row)]) }
+   end
 end
