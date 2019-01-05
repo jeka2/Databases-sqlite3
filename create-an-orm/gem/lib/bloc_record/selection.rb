6abfd1f0fb2887499097a6c5c16dda739b23ffe5
Yevgeniy Ivanov <anigeka@gmail.com>
2019-01-04 19:08:34 -0600

    selection

--- create-an-orm/gem/lib/bloc_record/selection.rb
+++ create-an-orm/gem/lib/bloc_record/selection.rb
@@ -1,6 +1,15 @@
 require 'sqlite3'
 
 module Selection
+  def all
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table};
+    SQL
+
+    rows_to_array(rows)
+
+  end
+
 	def find(*ids)
  
      if ids.length == 1
@@ -13,7 +22,16 @@ module Selection
  
        rows_to_array(rows)
      end
-    end
+  end
+
+  def find_by(attribute, value)
+     rows = connection.execute <<-SQL
+       SELECT #{columns.join ","} FROM #{table}
+       WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+     SQL
+ 
+     rows_to_array(rows)
+  end
 
 	def find_one(id)
 		row = connection.get_first_row <<-SQL
@@ -21,12 +39,59 @@ module Selection
 			WHERE id = #{id};
 		SQL
 
-		data = Hash[columns.zip(row)]
-		new(data)
+		init_object_from_row(row)
+
+  def take(num=1)
+      if num > 1
+        rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        ORDER BY random()
+        LIMIT #{num};
+      SQL
+ 
+      rows_to_array(rows)
+    else
+      take_one
+    end
+  end  
+
+ def first
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id ASC LIMIT 1;
+    SQL
+ 
+    init_object_from_row(row)
+ end
+ 
+ def last
+      row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id DESC LIMIT 1;
+   SQL
+ 
+   init_object_from_row(row)
+  end
+
+  def take_one
+    row = connection.get_first_row <<-SQL
+     SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT 1;
+    SQL
+ 
+    init_object_from_row(row)
+  end
 	end
 
 
    private
+   def init_object_from_row(row)
+     if row 
+      data = Hash[columns.zip(row)]
+      new(data)
+     end
+   end
  
    def rows_to_array(rows)
      rows.map { |row| new(Hash[columns.zip(row)]) }
