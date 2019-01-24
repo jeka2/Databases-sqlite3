18fdcc0ea8effd0c197aa4f9c2bd27eab5ffa033
Yevgeniy Ivanov <anigeka@gmail.com>
2019-01-24 11:47:22 -0600

    exercise complete

--- create-an-orm/address-bloc/db/seed.rb
+++ create-an-orm/address-bloc/db/seed.rb
@@ -6,5 +6,4 @@
 
  entries = {1 => {name: 'first_changed', phone_number: '333-333-3333'}, 2 => {phone_number: '999-999-9999'}, 3 => {name: 'third changed'}}
  e = Entry.where(id: 25)
- e.save
- Entry.where(name: 'name').where()
+ Entry.where(name: 'name').not(phone_number: "999-999-9999", email: "something@something.com")
--- create-an-orm/gem/lib/bloc_record/collection.rb
+++ create-an-orm/gem/lib/bloc_record/collection.rb
@@ -12,47 +12,60 @@ module BlocRecord
      end
 
      def where(*args)
-     	id_condition = ""
-     	self.each { |obj| id_condition.concat("#{obj.id} OR ") }
-     	id_condition = id_condition[0...-3]
-     	if args.count > 1
-      	 expression = args.shift
-      	 params = args
-    	else
-        case args.first
-        when String
-         expression = args.first
-        when Hash
-         expression_hash = BlocRecord::Utility.convert_keys(args.first)
-         expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+       key_holder = []
+       value_holder = []
+       record_holder = []
+       args.each do |pairs|
+        pairs.each do |key_value|
+          key_holder <<  key_value[0].to_s
+          value_holder << key_value[1]
         end
-        p expression
-     end
-     end
-   end
- end
-
-
-
-  def where(*args)
-    if args.count > 1
-       expression = args.shift
-       params = args
-    else
-       case args.first
-       when String
-         expression = args.first
-       when Hash
-         expression_hash = BlocRecord::Utility.convert_keys(args.first)
-         expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
        end
-    end
+       if !(key_holder - self[0].class.columns).empty?
+         p "One or more of the keys you provided aren't recognized by the database"
+         exit(0)
+       end
+       self.each do |obj|
+         matches_found = 0
+         0.upto(key_holder.length - 1) do |index|
+          if obj.send(key_holder[index]) === value_holder[index]
+            matches_found += 1 
+          end
+         end
+         if matches_found == key_holder.length
+          record_holder << obj
+         end
+       end
+       record_holder
+     end
 
-     sql = <<-SQL
-       SELECT #{columns.join ","} FROM #{table}
-       WHERE #{expression};
-     SQL
+     def not(*args)
+      key_holder = []
+       value_holder = []
+       record_holder = []
+       args.each do |pairs|
+        pairs.each do |key_value|
+          key_holder <<  key_value[0].to_s
+          value_holder << key_value[1]
+        end
+       end
+       if !(key_holder - self[0].class.columns).empty?
+         p "One or more of the keys you provided aren't recognized by the database"
+         exit(0)
+       end
+       self.each do |obj|
+         matches_found = 0
+         0.upto(key_holder.length - 1) do |index|
+          if obj.send(key_holder[index]) === value_holder[index]
+            matches_found += 1 
+          end
+         end
+         unless matches_found == key_holder.length
+          record_holder << obj
+         end
+       end
+       record_holder
+     end
+ end
+end
 
-     rows = connection.execute(sql, params)
-     rows_to_array(rows)
-   end
